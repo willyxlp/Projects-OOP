b8cd63204f4731112a881858c021a5d6fc006c71
William Panzo <150975677+willyxlp@users.noreply.github.com>
2024-05-31 19:28:20 +0200

    Add files via upload

---
+++ password checker/Form1.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.title = new System.Windows.Forms.Label();
+            this.powerlbl = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.insert = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.power_lbl = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.LightGray;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.Purple;
+            this.textBox1.Location = new System.Drawing.Point(52, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(493, 32);
+            this.textBox1.TabIndex = 98;
+            this.textBox1.UseSystemPasswordChar = true;
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
+            this.textBox1.TabStopChanged += new System.EventHandler(this.Form1_Load);
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.BackColor = System.Drawing.Color.Transparent;
+            this.title.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.title.Location = new System.Drawing.Point(-53, 148);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(386, 102);
+            this.title.TabIndex = 1;
+            this.title.Text = " \r\n            Password Checker   \r\n                  Pro edition \r\n";
+            // 
+            // powerlbl
+            // 
+            this.powerlbl.AutoSize = true;
+            this.powerlbl.BackColor = System.Drawing.Color.Transparent;
+            this.powerlbl.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.powerlbl.ForeColor = System.Drawing.Color.LightGray;
+            this.powerlbl.Location = new System.Drawing.Point(355, 273);
+            this.powerlbl.Name = "powerlbl";
+            this.powerlbl.Size = new System.Drawing.Size(149, 21);
+            this.powerlbl.TabIndex = 2;
+            this.powerlbl.Text = "Password Power:";
+            this.powerlbl.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Purple;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.GhostWhite;
+            this.button1.Location = new System.Drawing.Point(383, 324);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(215, 38);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "CHECK";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // insert
+            // 
+            this.insert.AutoSize = true;
+            this.insert.BackColor = System.Drawing.Color.Transparent;
+            this.insert.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.insert.ForeColor = System.Drawing.Color.Purple;
+            this.insert.Location = new System.Drawing.Point(364, 133);
+            this.insert.Name = "insert";
+            this.insert.Size = new System.Drawing.Size(295, 34);
+            this.insert.TabIndex = 11;
+            this.insert.Text = "Insert your password:";
+            this.insert.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.LightGray;
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox1.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.ForeColor = System.Drawing.Color.LightGray;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(404, 386);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(423, 144);
+            this.listBox1.TabIndex = 12;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.title);
+            this.panel1.Location = new System.Drawing.Point(-1, -5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(336, 608);
+            this.panel1.TabIndex = 2;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(107, 53);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(119, 118);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 7.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(13, 517);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 34);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Developed By\r\n     69994";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Purple;
+            this.button2.Location = new System.Drawing.Point(857, 9);
+            this.button2.Margin = new System.Windows.Forms.Padding(0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(64, 39);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "exit";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(9, 7);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(37, 39);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 17;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.White;
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.Purple;
+            this.button4.Location = new System.Drawing.Point(627, 324);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(200, 38);
+            this.button4.TabIndex = 19;
+            this.button4.Text = "SHOW STATS";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.LightGray;
+            this.panel2.Controls.Add(this.pictureBox3);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Location = new System.Drawing.Point(331, 186);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(619, 60);
+            this.panel2.TabIndex = 20;
+            this.panel2.Click += new System.EventHandler(this.panel2_Click);
+            this.panel2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseClick);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(554, 12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(26, 27);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 21;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            this.pictureBox3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox3_MouseDown);
+            this.pictureBox3.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox3_MouseUp);
+            // 
+            // power_lbl
+            // 
+            this.power_lbl.AutoSize = true;
+            this.power_lbl.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.power_lbl.ForeColor = System.Drawing.Color.LightGray;
+            this.power_lbl.Location = new System.Drawing.Point(510, 275);
+            this.power_lbl.Name = "power_lbl";
+            this.power_lbl.Size = new System.Drawing.Size(19, 19);
+            this.power_lbl.TabIndex = 21;
+            this.power_lbl.Text = "0";
+            this.power_lbl.Click += new System.EventHandler(this.power_lbl_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.White;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.Purple;
+            this.button3.Location = new System.Drawing.Point(627, 324);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(200, 38);
+            this.button3.TabIndex = 22;
+            this.button3.Text = "HIDE STATS\r\n";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(944, 603);
+            this.ControlBox = false;
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.power_lbl);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.insert);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.powerlbl);
+            this.Controls.Add(this.panel2);
+            this.ForeColor = System.Drawing.Color.Purple;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Password checker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Label powerlbl;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label insert;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label power_lbl;
+    }
+}
+
---
+++ password checker/Form1.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
+
+
+namespace WindowsFormsApp2
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+              button4.Visible = true;
+               button3.Visible = false;
+                listBox1.BorderStyle = BorderStyle.None;
+                listBox1.ForeColor = Color.LightGray;
+              listBox1.BackColor = Color.LightGray;
+             powerlbl.ForeColor = Color.Purple;
+            power_lbl.ForeColor = Color.White;
+
+
+
+            int count = 0;
+            if (textBox1.TextLength >= 15)
+            {  
+                count += 75;
+            }
+            else if (textBox1.TextLength >= 10 && textBox1.TextLength <= 14)
+            { 
+               count += 60;
+            }
+            else if (textBox1.TextLength >= 7 && textBox1.TextLength <= 9)
+            {
+                count += 40;
+            }
+            else if (textBox1.TextLength > 4)
+            {
+                count += 20;
+                listBox1.Items.Add("password is too short");
+            }
+            else
+            {
+                count -= 100;
+                listBox1.Items.Add("password is too short");
+            }
+            
+            if (Regex.IsMatch(textBox1.Text, @"[a-zA-Z]")) 
+            {  
+                count += 15;
+            }
+            if (Regex.IsMatch(textBox1.Text, @"\d"))
+            {
+                count += 10;
+            }
+
+            if (Regex.IsMatch(textBox1.Text, @"[!@#$%^&*()_+=\[{\]};:<>|./?,-]"))
+            {
+
+                count += 15;
+            }
+
+
+
+
+
+            if (count >= 90)
+            {
+                power_lbl.ForeColor = Color.Green;
+                power_lbl.Text = ("Very Strong");
+                 
+            }
+            else if (count >= 75 && count <= 89) 
+            {
+                power_lbl.ForeColor = Color.DarkGreen;
+                power_lbl.Text = ("Strong");
+                 
+            }
+            else if (count >= 50 && count <= 74)
+            {
+                power_lbl.Text = ("Medium");
+                power_lbl.ForeColor = Color.Orange;
+            }
+                 
+
+            else if (count >= 25 && count <= 49)
+            {
+                power_lbl.ForeColor = Color.Red;
+                power_lbl.Text = ("Weak");
+                 
+            }
+
+            else
+            {
+                power_lbl.ForeColor = Color.DarkRed;
+                power_lbl.Text = ("Very Weak");
+
+            }
+             
+
+             
+
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit
+                ();
+        }
+
+        private void panel2_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void panel2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            textBox1.BackColor = Color.White;
+             panel2.BackColor = Color.White;
+            textBox1.BorderStyle= BorderStyle.None;
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+            
+
+
+        }
+
+        private void pictureBox3_MouseDown(object sender, MouseEventArgs e)
+        {
+            textBox1.UseSystemPasswordChar = false;
+             
+        }
+
+        private void pictureBox3_MouseUp(object sender, MouseEventArgs e)
+        {
+            textBox1.UseSystemPasswordChar = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            textBox1.BackColor = Color.LightGray;
+             panel2.BackColor = Color.LightGray;
+             textBox1.BorderStyle = BorderStyle.FixedSingle;
+            this.ActiveControl = null;
+        }
+
+        private void power_lbl_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.BackColor = Color.White;
+             listBox1.ForeColor = Color.Purple;
+              listBox1.BorderStyle = BorderStyle.FixedSingle;
+             button3.Visible = true;
+            button4.Visible = false;
+
+            if (Regex.IsMatch(textBox1.Text, @"[a-zA-Z]") && Regex.IsMatch(textBox1.Text, @"\d") && Regex.IsMatch(textBox1.Text, @"[!@#$%^&*()_+=\[{\]};:<>|./?,-]") && textBox1.TextLength >= 7)
+            {
+                listBox1.Items.Add("Your Password dont have anny issues");
+            }          
+            else if (Regex.IsMatch(textBox1.Text, @"[a-zA-Z]") && Regex.IsMatch(textBox1.Text, @"\d"))
+            {
+                listBox1.Items.Add("Your password dont have any special character");
+            }
+            else if (Regex.IsMatch(textBox1.Text, @"[a-zA-Z]") && Regex.IsMatch(textBox1.Text, @"[!@#$%^&*()_+=\[{\]};:<>|./?,-]"))
+            {
+                listBox1.Items.Add("Your password dont have any number");
+            }
+            else if (Regex.IsMatch(textBox1.Text, @"\d") && Regex.IsMatch(textBox1.Text, @"[!@#$%^&*()_+=\[{\]};:<>|./?,-]"))
+            {
+                listBox1.Items.Add("your password dont have any letters");
+            }
+            else if (Regex.IsMatch(textBox1.Text, @"[a-zA-Z]"))
+            {
+                listBox1.Items.Add("Your password dont have any number");
+                listBox1.Items.Add("Your password dont have any special character");
+            }
+            else if (Regex.IsMatch(textBox1.Text, @"\d"))
+            {
+                listBox1.Items.Add("your password dont have any letters");
+                listBox1.Items.Add("Your password dont have any special character");
+            }
+            else if (Regex.IsMatch(textBox1.Text, @"[!@#$%^&*()_+=\[{\]};:<>|./?,-]"))
+            {
+                listBox1.Items.Add("your password dont have any letters");
+                listBox1.Items.Add("Your password dont have any number");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+             button4.Visible = true;
+               button3.Visible=false;
+               listBox1.BorderStyle = BorderStyle.None;
+              listBox1.ForeColor= Color.LightGray;
+            listBox1.BackColor= Color.LightGray;        
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+             
+        }
+    }
+}
